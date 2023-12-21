024a6c92991838fbe43e6bb13338693464ab360c
Trill3332 <bnc3332@gmail.com>
2023-12-21 20:20:32 +0800

    add library project

---
+++ Library/main.js
@@ -0,0 +1,7 @@
+const addBookBtn = document.querySelector("newBook");
+const form = document.getElementById("showForm");
+const modal = document.querySelector("modal");
+const close = document.querySelector("close");
+
+
+//
