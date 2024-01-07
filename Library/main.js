bce3bc0bb27dd1993f442fccd3b8aa80ad09fb1b
Trill3332 <bnc3332@gmail.com>
2024-01-07 19:56:30 +0800

    add

--- Library/main.js
+++ Library/main.js
@@ -1,43 +1,52 @@
 let newBook;
 const addBookBtn = document.getElementById('addBookBtn')
 const form = document.getElementById('addBookForm')
-const bookContainer = document.querySelector('.books-container')
+const bookContainer = document.getElementById('books-container')
 const overlay = document.getElementById('overlay')
+
 //constructor
 const myLibrary = [];
-function Book(title,author,pages,read = false) {
+function Book(title, author, pages, read = false) {
     this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+        this.author = author,
+        this.pages = pages,
+        this.read = read
 }
 
-addBookBtn.addEventListener('click',addOverlay);
+addBookBtn.addEventListener('click', addOverlay);
 function addOverlay() {
     form.classList.add('active');
     overlay.style.display = 'block';
 }
-window.addEventListener('click' , (event) => {
-    if(event.target !== form && event.target !== addBookBtn) {
+window.addEventListener('click', (event) => {
+    if (!form.contains(event.target) && event.target !== addBookBtn) {
         form.classList.remove('active');
         overlay.style.display = 'none'
     }
 })
-form.addEventListener('submit',(event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    let formValue = event.target.elements;
-    newBook = new Book(
-        formValue.title,
-        formValue.author,
-        formValue,pages,
-        formValue.checked
-    )
-    
+
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let isRead = document.getElementById('is-read').checked;
+
+    newBook = new Book(title, author, pages, isRead)
+
     myLibrary.push(newBook)
-
-
+    renderBook();
+    form.classList.remove('active');
+    overlay.style.display = 'none'
 })
 
 function renderBook() {
-    
+    myLibrary.forEach((book) => {
+        let div = document.createElement('div');
+        div.innerHTML = `Title : ${book.title}<br>
+                         Author : ${book.author}<br>
+                         Pages : ${book.pages}<br>
+                         Read : ${book.read ? 'Yes' : 'No'}<br>`;
+        bookContainer.appendChild(div)
+    })
 }
